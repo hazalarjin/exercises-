b960189799ba8c25dd59c8cd110a6d941335aefe
hazalarjin <160840876+hazalarjin@users.noreply.github.com>
2024-03-24 20:45:17 +0300

    Add files via upload

---
+++ ciftSayi.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class ciftSayi {
+	public static void main(String [] args) {
+		/*Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 
+		  3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+		 */
+		Scanner scanner = new Scanner(System.in); {
+			System.out.println("Bir sayı giriniz");
+			int sayi = scanner.nextInt();
+			
+			for(int i = 0; i<=sayi ; i++) {
+				if(i%3==0 && i%4==0) {
+					System.out.println(i);
+				}
+					}
+				
+				}
+			}
+		}
+	
+
+
